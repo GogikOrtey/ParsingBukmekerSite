de387621f24c54b4b23b1579b71c7982f0099eff
Gogik <gog.ortey@yandex.ru>
2025-01-29 17:05:38 +0500

    2 - Корректное нажатие на первый элемент раскрытия списка

--- index.js
+++ index.js
@@ -23,11 +23,22 @@ function sleep(ms) {
   // Открываем указанную страницу и ждем полной загрузки
   await page.goto('https://betboom.ru/sport/football', { waitUntil: 'load' });
 
-  // Ожидание 5 секунд
-  await sleep(5000);
+  // // Ожидание 5 секунд
+  // await sleep(5000);
 
-  // После полной загрузки страницы выводим сообщение в консоль
-  console.log('Страница полностью загружена!');
+  // // После полной загрузки страницы выводим сообщение в консоль
+  // console.log('Страница полностью загружена!');
+
+  // // Нажатие на первый элемент с классом 'h4qas-a84e8c10'
+  // await page.click('.h4qas-a84e8c10');
+
+  // Ожидание загрузки элементов с классом 'h4qas-a84e8c10'
+  await page.waitForSelector('.h4qas-a84e8c10');
+
+  // Прокрутка элемента в видимую область и нажатие на него
+  const element = await page.$('.h4qas-a84e8c10');
+  await element.evaluate(el => el.scrollIntoView());
+  await element.click();
 
   // Закрываем браузер (по желанию)
   // await browser.close();
