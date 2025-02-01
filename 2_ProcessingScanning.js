25cc2b246b47311b44f4b51aaad7decc875d930f
Gogik <gog.ortey@yandex.ru>
2025-02-01 15:25:56 +0500

    33 Добавил асинхронное выполнение кода, таймер работает корректно

--- 2_ProcessingScanning.js
+++ 2_ProcessingScanning.js
@@ -1,3 +1,8 @@
+const { promisify } = require('util');
+const { exec } = require('child_process');
+const execPromise = promisify(exec);
+
+
 /*                                ОПИСАНИЕ:
 
 Этот скрипт управляет другими скриптами:
@@ -12,21 +17,45 @@
 */
 
 
+
+
+
+
 //
 // Запускаю сприпт 1_Scanning.js
 //
 
-const { exec } = require('child_process');
 
-exec('node 1_Scanning.js', (error, stdout, stderr) => {
-    if (error) {
+
+
+// Открываем пространство асинхронного выполнения кода
+// т.е. только последовательного:
+(async () => {
+    const start = Date.now();
+
+    try {
+        // Запускаем скрипт
+        const { stdout, stderr } = await execPromise('node 1_Scanning.js');
+        
+        // Вычисляем таймер
+        const duration = (Date.now() - start) / 1000;
+
+        // Выводим в консоль этого скрипта логи того скрипта
+        console.log('Сканирование завершено: 1_Scanning.js завершился');
+        console.log(`Лог этого скрипта: ${stdout}`);
+        // console.error(`Ошибки этого скрипта: ${stderr}`);
+        
+        // Выводим время выполнения скрипта
+        if (duration > 120) {
+            console.log(`Время выполнения сканирования: ${(duration / 60).toFixed(1)} минут`);
+        } else {
+            console.log(`Время выполнения сканирования: ${duration.toFixed(0)} секунд`);
+        }
+
+    } catch (error) {
         console.error(`exec error: ${error}`);
-        return;
     }
-    console.log(`Лог этого скрипта: ${stdout}`);
-    console.error(`Ошибки этого сприпта: ${stderr}`);
-    console.log('Сканирование завершено: 1_Scanning.js завершился');
-});
+})();
 
 
 
@@ -36,6 +65,20 @@ exec('node 1_Scanning.js', (error, stdout, stderr) => {
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+// Проверить на обработку ошибок
+
+// Добавить выгрузку логов в текстовый файл, каждый раз
 
 
 
