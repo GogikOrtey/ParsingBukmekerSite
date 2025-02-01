e42cb4013a96d3ede563937fecec4a84ad8dd621
Gogik <gog.ortey@yandex.ru>
2025-02-01 16:20:16 +0500

    36 Ошибки корректно обрабатываются, а также заносятся в логи

--- 2_ProcessingScanning.js
+++ 2_ProcessingScanning.js
@@ -33,35 +33,55 @@ const path = require('path');
 
 // Открываем пространство асинхронного выполнения кода
 // т.е. только последовательного:
+
 (async () => {
-    const start = Date.now();
+    // Макисмальное количество попыток запуска скрипта загрузки
+    const maxRetries = 7; 
+    // Текущее количество попыток
+    let attempt = 0;
 
-    try {
-        // Запускаем скрипт
-        const { stdout, stderr } = await execPromise('node 1_Scanning.js');
-        
-        // Вычисляем таймер
-        const duration = (Date.now() - start) / 1000;
+    let errorLogs = "";
 
-        // Выводим в консоль этого скрипта логи того скрипта
-        console.log('Сканирование завершено: 1_Scanning.js завершился');
-        console.log(`Лог этого скрипта: ${stdout}`);
-        // console.error(`Ошибки этого скрипта: ${stderr}`);
-        
-        // Выводим время выполнения скрипта
-        if (duration > 120) {
-            console.log(`Время выполнения сканирования: ${(duration / 60).toFixed(1)} минут`);
-        } else {
-            console.log(`Время выполнения сканирования: ${duration.toFixed(0)} секунд`);
+    while (attempt < maxRetries) {
+        attempt++;
+        const start = Date.now();
+
+        try {
+            // Запускаем скрипт
+            const { stdout, stderr } = await execPromise('node 1_Scanning.js');
+            
+            // Вычисляем таймер
+            const duration = (Date.now() - start) / 1000;
+
+            // Выводим в консоль этого скрипта логи того скрипта
+            console.log('Сканирование завершено: 1_Scanning.js завершился');
+            console.log(`Лог этого скрипта: ${stdout}`);
+            // console.error(`Ошибки этого скрипта: ${stderr}`);
+            
+            // Выводим время выполнения скрипта
+            if (duration > 120) {
+                console.log(`Время выполнения сканирования: ${(duration / 60).toFixed(1)} минут`);
+            } else {
+                console.log(`Время выполнения сканирования: ${duration.toFixed(0)} секунд`);
+            }
+
+            await SaveLogsFromTxtFile(stdout, stderr, duration);
+
+            // Если скрипт завершился успешно, выходим из цикла
+            break;
+        } catch (error) {
+            console.error(`Попытка ${attempt} не удалась: ${error}`);
+            errorLogs += error;
+
+            // Если это была последняя попытка, выбрасываем ошибку
+            if (attempt === maxRetries) {
+                console.error('Скрипт завершился с ошибкой после 5 попыток');
+                SaveLogsFromTxtFile("", "", 0);
+                throw error;
+            }
         }
-
-        await SaveLogsFromTxtFile(stdout, stderr, duration);
-
-    } catch (error) {
-        console.error(`exec error: ${error}`);
     }
 
-    // Вот здесь 5 раз пробовать ещё раз запустить этот скрипт, если он звершается с ошибкой
 
 
 
@@ -81,6 +101,9 @@ const path = require('path');
         // Устанавливаем имя и путь для создания этого файла
         const logFileName = `LoadingLog_${formattedDate}.txt`;
         const logFilePath = path.join(logDir, logFileName);
+
+        // Добавляю ошибки, которые возникли в результате запуска скрипта
+        stdout = errorLogs + stdout;
 
         const logData = `
         Сканирование завершено: 1_Scanning.js завершился
